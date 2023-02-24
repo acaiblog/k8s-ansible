612c9b40e309b4ec1aeaa2807410569a14d65d48
acaiblog <baocai.guo@qq.com>
2023-02-24 11:39:14 +0800

    support containerd deploy k8s

--- deploy.py
+++ deploy.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import argparse
 import logging
 import os
@@ -8,23 +9,18 @@ def args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--action', type=str, required=True, help='接受deploy/upgrade/destroy参数部署/升级/清理k8s集群')
     args = parser.parse_args().__dict__
-    logging.debug('args: %s' %args)
     return args
 
-def cmd(action):
-    logging.debug("action: %s" %action)
-    if action != 'destroy':
-      os.system('ansible-playbook  -i inventory/hosts -e "action=%s" os-init.yaml' %action)
-      os.system('ansible-playbook  -i inventory/hosts -e "action=%s" k8s.yaml -v' %action)
-    elif action == 'destroy':
-      os.system('ansible-playbook -i inventory/hosts k8s-clear.yaml')
+def deploy():
+    os.system('ansible-playbook  -i inventory/hosts -e "k8s_action=%s" os-init.yaml' %action)
+    os.system('ansible-playbook  -i inventory/hosts -e "k8s_action=%s" k8s.yaml' %action)
+
+def upgrade():
+    os.system('ansible-playbook  -i inventory/hosts -e "k8s_action=%s" k8s.yaml' %action)
 
 def destroy():
     os.system('ansible-playbook -i inventory/hosts k8s-clear.yaml')
 
 if __name__ == "__main__":
     action = args()['action']
-    if action == 'deploy' or 'upgrade':
-      cmd(action)
-    elif action == 'destroy':
-      cmd(action)
+    locals()[action]()
